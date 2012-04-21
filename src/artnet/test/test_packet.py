2d5c673827322ac0a823f20c7ca353a87b34cda5
Phil Christensen <pchristensen@freelancersunion.org>
2012-04-21 18:15:08 -0400

    seem to be sending the same stuff as other apps, but not getting any results

--- src/artnet/test/test_packet.py
+++ src/artnet/test/test_packet.py
@@ -2,9 +2,29 @@ import unittest, copy
 
 from artnet import packet
 
+POLL_PACKET = ''.join([
+	"Art-Net\x00",  # ID[8] (Int8)
+	"\x00\x20",     # OpCode (Int16)
+	"\x00", "\x0e", # ProtVerHi, ProtVerLo (Int8)
+	"\x02",         # TalkToMe (Int8)
+	"\x00",         # Priority (Int8)
+])
+
+TOD_REQUEST = ''.join([
+	"Art-Net\x00",           # ID[8] (Int8)
+	"\x00\x80",              # OpCode (Int16)
+	"\x00", "\x0e",          # ProtVerHi, ProtVerLo (Int8)
+	"\x00",  "\x00",         # Filler1, Filler2 (Int8)
+	"".join(["\x00"] * 7),   # Spare1-7 (Int8)
+	"\x00", # Net (Int8)
+	"\x00", # Command (Int8)
+	"\x00", # AddCount (Int8)
+	"\x00", # AddCount (Int8)
+])
+
 BLACKOUT_PACKET = ''.join([
 	"Art-Net\x00",  # ID[8] (Int8)
-	"P\x00",        # OpCode (Int16)
+	"\x00P",        # OpCode (Int16)
 	"\x00", "\x0e", # ProtVerHi, ProtVerLo (Int8)
 	"\x00",         # Sequence (Int8)
 	"\x00",         # Physical (Int8)
@@ -15,18 +35,18 @@ BLACKOUT_PACKET = ''.join([
 
 U30_BLACKOUT_PACKET = ''.join([
 	"Art-Net\x00",  # ID[8] (Int8)
-	"P\x00",        # OpCode (Int16)
+	"\x00P",        # OpCode (Int16)
 	"\x00", "\x0e", # ProtVerHi, ProtVerLo (Int8)
 	"\x00",         # Sequence (Int8)
 	"\x00",         # Physical (Int8)
-	"\x1e", "\x00", # SubUni, Net (Int8)
+	"\x00", "\x1e", # SubUni, Net (Int8)
 	"\x02", "\x00", # LenHi, LenLo (Int8)
 	"".join(["\x00"] * 512)
 ])
 
 FIRST_FIXTURE_WHITE_PACKET = ''.join([
 	"Art-Net\x00",  # ID[8] (Int8)
-	"P\x00",        # OpCode (Int16)
+	"\x00P",        # OpCode (Int16)
 	"\x00", "\x0e", # ProtVerHi, ProtVerLo (Int8)
 	"\x00",         # Sequence (Int8)
 	"\x00",         # Physical (Int8)
@@ -61,6 +81,18 @@ class TestPacket(unittest.TestCase):
 		x = p.encode()
 		self.assertEqual(len(x), len(b))
 		self.assertEqual(x, b)
+	
+	def test_poll(self):
+		p = packet.ArtNetPacket(opcode=packet.ARTNET_POLL)
+		x = p.encode()
+		self.assertEqual(len(x), len(POLL_PACKET))
+		self.assertEqual(x, POLL_PACKET)
+	
+	def test_tof_request(self):
+		p = packet.ArtNetPacket(opcode=packet.ARTNET_TOD_REQUEST)
+		x = p.encode()
+		self.assertEqual(len(x), len(TOD_REQUEST))
+		self.assertEqual(x, TOD_REQUEST)
 	
 	def test_blackout(self):
 		p = packet.ArtNetPacket()
