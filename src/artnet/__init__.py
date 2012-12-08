132084ec4ca25dab7e300f0ef8407bd053a767c8
Phil Christensen <pchristensen@freelancersunion.org>
2012-12-08 09:31:56 -0500

    tweaks and fixes, now using broadcast

--- src/artnet/__init__.py
+++ src/artnet/__init__.py
@@ -4,10 +4,12 @@ from artnet import packet
 
 def send_dmx(address, channels):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.bind(('', 6454))
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+	sock.bind(('', packet.ARTNET_PORT))
 	
 	p = packet.DmxPacket()
-	for index in len(channels):
+	for index in range(len(channels)):
 		p[index] = channels[index]
+	
 	sock.sendto(p.encode(), (address, packet.ARTNET_PORT))
+	sock.close()
--- src/artnet/scripts/halfup.py
+++ src/artnet/scripts/halfup.py
@@ -4,8 +4,7 @@ import artnet
 
 def main():
 	if(len(sys.argv) < 2):
-		print "\nUsage:\n\tartnet_halfup [address]\n"
-		sys.exit(1)
+		sys.argv.append('<broadcast>')
 	
 	artnet.send_dmx(sys.argv[1], [127] * 512)
 
