182bf65061422e044aec920f224968da7b191426
Phil Christensen <pchristensen@freelancersunion.org>
2013-01-25 11:34:07 -0500

    added some comments

--- src/artnet/scripts/layers_example.py
+++ src/artnet/scripts/layers_example.py
@@ -3,6 +3,7 @@ import time
 from artnet import dmx
 from artnet.logical import fixtures
 
+# set up test fixtures
 g = fixtures.FixtureGroup([
 	fixtures.Fixture.create(420, 'chauvet/slimpar-64.yaml'),
 	fixtures.Fixture.create(427, 'chauvet/slimpar-64.yaml'),
@@ -11,19 +12,30 @@ g = fixtures.FixtureGroup([
 ])
 
 def all_red():
+	"""
+	A simple all-red light generator.
+	"""
 	while(True):
 		g.setColor('#ff0000')
 		g.setIntensity(255)
 		yield g.getChannels()
 
 def single_white_beat_chase(clock, secs=5.0):
+	"""
+	A simple white chase pattern.
+	"""
 	t = time.time()
 	c = clock()
 	while(c['running']):
+		# Reset to white, but blacked out
 		g.setColor('#ffffff')
 		g.setIntensity(0)
+		
+		# Grab one fixture, set its intensity
 		f = g.fixtures[c['beat'] - 1]
 		f.setIntensity(255)
+		
+		# Grab just that fixture's DMX values
 		frame = f.getChannels()
 		yield frame
 		if(secs and time.time() - t >= secs):
@@ -32,7 +44,9 @@ def single_white_beat_chase(clock, secs=5.0):
 
 def main(address):
 	q = dmx.Controller(address, bpm=60, nodaemon=True, runout=True)
+	# "base color" red
 	q.add(all_red())
+	# white chase layer
 	q.add(single_white_beat_chase(q.get_clock()))
 	q.start()
 
